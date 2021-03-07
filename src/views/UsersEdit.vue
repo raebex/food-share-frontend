317153a9df98fe540288b658d1fdd29dca77df06
rebecca <r.young1025@gmail.com>
2021-03-06 22:25:22 -0600

    basic user edit

--- src/views/UsersEdit.vue
+++ src/views/UsersEdit.vue
@@ -1,5 +1,96 @@
 <template>
   <div class="users-edit">
-    Hello World
+    <form v-on:submit.prevent="update()">
+      <h1>Edit Profile</h1>
+      <ul>
+        <li class="text-danger" v-for="error in errors" v-bind:key="error">
+          {{ error }}
+        </li>
+      </ul>
+      <div class="form-group">
+        <label>First Name:</label>
+        <input type="text" class="form-control" v-model="user.first_name" />
+      </div>
+      <div class="form-group">
+        <label>Last Name:</label>
+        <input type="text" class="form-control" v-model="user.last_name" />
+      </div>
+      <div class="form-group">
+        <label>Email:</label>
+        <input type="email" class="form-control" v-model="user.email" />
+      </div>
+      <div class="form-group">
+        <label>Password:</label>
+        <input type="password" class="form-control" v-model="user.password" />
+      </div>
+      <div class="form-group">
+        <label>Password confirmation:</label>
+        <input type="password" class="form-control" v-model="user.password_confirmation" />
+      </div>
+      <div class="form-group">
+        <label>Image URL:</label>
+        <input type="text" class="form-control" v-model="user.image_url" />
+      </div>
+      <div class="form-group">
+        <label>Phone number:</label>
+        <input type="text" class="form-control" v-model="user.phone" />
+      </div>
+      <div class="form-group">
+        <label>Address:</label>
+        <input type="text" class="form-control" v-model="user.address" />
+      </div>
+      <div class="form-group">
+        <label>Bio:</label>
+        <textarea type="text" class="form-control" v-model="user.bio"></textarea>
+      </div>
+      <div class="form-group">
+        <label>Chef?</label>
+        <input type="checkbox" class="form-control" v-model="user.chef" />
+      </div>
+      <input type="submit" class="btn btn-primary" value="Save" />
+    </form>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      user: {},
+      errors: [],
+    };
+  },
+  created: function() {
+    axios.get(`/api/users/${this.$route.params.id}`).then(response => {
+      this.user = response.data;
+    });
+  },
+  methods: {
+    update: function() {
+      var params = {
+        first_name: this.user.first_name,
+        last_name: this.user.last_name,
+        email: this.user.email,
+        password: this.user.password,
+        password_confirmation: this.user.password_confirmation,
+        image_url: this.user.image_url,
+        phone: this.user.phone,
+        address: this.user.address,
+        bio: this.user.bio,
+        chef: this.user.chef,
+      };
+      axios
+        .patch(`/api/users/${this.user.id}`, params)
+        .then(response => {
+          console.log(response.data);
+          this.$router.push(`/users/${this.user.id}`);
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
+};
+</script>
