8b73aad0340d58ca733cb58e4e48a2a47701a79e
rebecca <r.young1025@gmail.com>
2021-03-22 12:01:46 -0500

    implement theme on orders index page

--- src/views/OrdersIndex.vue
+++ src/views/OrdersIndex.vue
@@ -1,15 +1,53 @@
 <template>
   <div class="orders-index">
-    <ul v-for="order in orders" :key="order.id">
-      <li>
-        <router-link :to="`/orders/${order.id}`">
-          <h3>Order # {{ order.id }}</h3>
-          <p>{{ $parent.formattedDate(order.created_at) }}</p>
-          <p>{{ order.total | currency }}</p>
-          <p>From Chef {{ order.chef.first_name }}</p>
-        </router-link>
-      </li>
-    </ul>
+    <div class="container shadow-sm bg-white p-4 h-100">
+      <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="orders" role="tabpanel" aria-labelledby="orders-tab">
+          <h4 class="font-weight-bold mt-0 mb-4">Past Orders</h4>
+          <div v-for="order in orders" :key="order.id" class="bg-white card mb-4 order-list shadow-sm">
+            <div class="gold-members p-4">
+              <router-link :to="`/orders/${order.id}`">
+                <div class="media">
+                  <img class="mr-4" :src="order.chef.image_url" alt="Generic placeholder image" />
+                  <div class="media-body">
+                    <span class="float-right text-info">
+                      Completed on {{ $parent.formattedDate(order.ready_time) }} <i class="icofont-check-circled text-success"></i>
+                    </span>
+                    <h6 class="mb-2 text-black">
+                      Order from Chef&nbsp;
+                      {{ order.chef.first_name }}
+                    </h6>
+                    <p class="text-gray mb-1">
+                      <i class="icofont-location-arrow"></i>
+                      {{ order.chef.address.city }}, {{ order.chef.address.state }}
+                    </p>
+                    <p class="text-gray mb-3">
+                      <i class="icofont-list"></i>
+                      &nbsp;ORDER #{{ order.id }}&nbsp;
+                      <i class="icofont-clock-time ml-2"></i>
+                      &nbsp;{{ $parent.formattedDate(order.created_at) }}
+                    </p>
+                    <p class="text-dark">
+                      <span v-for="(dish, index) in order.dishes" :key="dish.id">
+                        <span v-if="index != 0">,&nbsp;</span>
+                        {{ dish.name }} x {{ dish.quantity }}
+                      </span>
+                    </p>
+                    <hr />
+                    <p class="mb-0 text-black text-primary pt-2">
+                      <span class="text-black font-weight-bold">
+                        &nbsp;Total Paid:
+                      </span>
+                      &nbsp;{{ order.total | currency }}
+                    </p>
+                  </div>
+                </div>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
